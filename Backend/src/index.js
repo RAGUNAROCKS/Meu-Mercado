0ddfae7746c6b7b4f33bd5270b1ee2dd7926aa9f
RAGUNAROCKS <https://github.com/RAGUNAROCKS/be-the-hero.git>
2022-05-15 11:11:35 -0300

    Meu Mercado - Aula 12

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,12 +4,15 @@ import controllerUsuarios from './controllers/controller.usuarios.js';
 import controllerMercados from './controllers/controller.mercados.js';
 import controllerProdutos from './controllers/controller.produtos.js';
 import controllerPedidos from './controllers/controller.pedidos.js';
+import BasicAuth from './config/basic-auth.js';
 
 const app = express();
 //Middleware JSON
 app.use(express.json());
 //Middleware CORS
 app.use(cors());
+//Basic Auth
+app.use(BasicAuth);
 //Controladores
 app.use(controllerUsuarios);
 app.use(controllerMercados);
